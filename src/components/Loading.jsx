3e809700fac743f1cb9128728d3cd90fe97608ab
AskatAsh <abdulaskat@gmail.com>
2024-12-25 20:18:32 +0600

    added loading component with daisyUI loading classes

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+    return (
+        <div className="text-center py-20 md:py-28">
+            <span className="loading loading-bars loading-lg text-green-700 dark:text-green-600"></span>
+        </div>
+    );
+};
+
+export default Loading;
