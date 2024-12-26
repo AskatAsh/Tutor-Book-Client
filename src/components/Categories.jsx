f94f2c9a79c672f74c5925a0073a8428a011beba
AskatAsh <abdulaskat@gmail.com>
2024-12-26 18:55:15 +0600

    changed code in categories section by creating categories array of language and icon

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,18 @@
-import { MdArrowForwardIos, MdOutlineCategory } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 import { Link } from "react-router-dom";
-import useGetCategories from "../hooks/useGetCategories";
-import Loading from "./Loading";
-import ErrorMessages from "./ErrorMessages";
 
 const Categories = () => {
-  const [categories, isLoading, errorMessage, getCategories] =
-    useGetCategories();
+  const categories = [
+    { language: "english", icon: "https://i.ibb.co.com/fGLX4yB/image.png" },
+    { language: "spanish", icon: "https://i.ibb.co.com/xjt3yVG/image.png" },
+    { language: "french", icon: "https://i.ibb.co.com/mDJxZ8H/image.png" },
+    { language: "german", icon: "https://i.ibb.co.com/Ltt5DKR/image.png" },
+    { language: "italian", icon: "https://i.ibb.co.com/R24z37G/image.png" },
+    { language: "chinese", icon: "https://i.ibb.co.com/gV8tnLF/image.png" },
+    { language: "arabic", icon: "https://i.ibb.co.com/KLD4NGT/image.png" },
+    { language: "japanese", icon: "https://i.ibb.co.com/d7dKpqJ/image.png" },
+    { language: "portuguese", icon: "https://i.ibb.co.com/xsctxVS/image.png" },
+  ];
 
   return (
     <section className="my-20">
@@ -14,36 +20,28 @@ const Categories = () => {
         Explore Categories
       </h2>
 
-      {isLoading ? (
-        <Loading />
-      ) : errorMessage.message ? (
-        <ErrorMessages
-          errorMessage={errorMessage}
-          onRetry={() => getCategories()}
-        />
-      ) : (
-        <div className="max-w-7xl w-11/12 mx-auto rounded-lg grid grid-cols-12 gap-5 my-10">
-          {categories.map((category, idx) => (
-            <Link to={`/findTutors/${category}`}
-              key={idx}
-              className="bg-white dark:bg-gray-800 p-4 rounded-lg col-span-4 border flex items-center justify-between dark:border-gray-700 hover:bg-green-100 dark:hover:bg-gray-700"
-            >
-              <div className="flex items-center gap-4">
-                <MdOutlineCategory size={28} />
-                <div>
-                  <strong className="text-xl font-semibold text-gray-700 dark:text-gray-300 inline-block">
-                    {category} Tutors
-                  </strong>
-                  <p className="text-xs text-gray-600 dark:text-gray-400">
-                    20,000+ Teachers
-                  </p>
-                </div>
+      <div className="max-w-7xl w-11/12 mx-auto rounded-lg grid grid-cols-12 gap-5 my-10">
+        {categories.map((category, idx) => (
+          <Link
+            to={`/findTutors/${category.language}`}
+            key={idx}
+            className="bg-white dark:bg-gray-800 p-4 rounded-lg col-span-4 border flex items-center justify-between dark:border-gray-700 hover:bg-green-100 dark:hover:bg-gray-700"
+          >
+            <div className="flex items-center gap-4">
+              <img className="w-10" src={category.icon} alt={`icon of ${category.language} category`} />
+              <div>
+                <strong className="text-xl font-semibold text-gray-700 dark:text-gray-300 inline-block">
+                {category.language.charAt(0).toUpperCase() + category.language.slice(1)} Tutors
+                </strong>
+                <p className="text-xs text-gray-600 dark:text-gray-400">
+                  20,000+ Teachers
+                </p>
               </div>
-              <MdArrowForwardIos size={20} />
-            </Link>
-          ))}
-        </div>
-      )}
+            </div>
+            <MdArrowForwardIos size={20} />
+          </Link>
+        ))}
+      </div>
     </section>
   );
 };
